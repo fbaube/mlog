84d38f5d34e0ae2741db802575a367cc34b4aa85
Fred Baube <fbaube@welho.com>
2022-02-16 09:09:05 +0200

    Better docu re RFC5424

--- emojis.go
+++ emojis.go
@@ -1,6 +1,7 @@
 package log
 
-// Success, Warning, Error can also be summary items.
+// (R,Y,G) (3,4,5) (Error,Warning,Okay) can
+// be summary items for execution checkpoints.
 // Grn, Ylw, Red are calm B/G indicator lights .
 const (
 	// NOTIFICATION / SUMMARY
@@ -17,15 +18,17 @@ const (
 	Ylw = "🟡"
 	Grn = "🟢"
 	*/
-) /* RFC5424:
-0 Emergency (system is unusable)
-1 Alert (take action ASAP)
-2 Critical
-3 Error
-4 Warning
-5 Notice (normal but significant condition)
-6 Informational
-7 Debug
+)
+
+/*  This    RFC5424
+0   -      Emergency (system is unusable)
+1   -      Alert (take action ASAP)
+2  Panic   Critical
+3  Error   Error
+4  Warning Warning
+5  Okay    Notice (normal but significant condition)
+6  Info    Informational
+7  Debug   Debug
 */
 
 func EmojiOfLevel(L Level) string {
@@ -49,6 +52,7 @@ func EmojiOfLevel(L Level) string {
 }
 
 /*
+Stockpile of useful emojis:
 ⭕ ✅ ❌ ❎
 🔴 🟠 🟡 🟢 🔵 🟣 🟤 ⚫ ⚪
 🟥 🟧 🟨 🟩 🟦 🟪 🟫 ⬛ ⬜ ◾ ◽
